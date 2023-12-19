added stratification, fixed f1, no label masking, and logging separate metrics for each feat.

dataset.py
@@ -17,6 +17,7 @@ class UNSW_NB15(Dataset):
     def __init__(self, data_csv, dtype_xlsx, num_clusters): 
         self.num_clusters = num_clusters
         self.feat_legend = pd.read_excel(dtype_xlsx, header=0, engine='openpyxl')
+        names = self.feat_legend['Name'].str.lower()
 
         if os.path.exists('processed_data/meta.pkl'):
             meta = pickle.load(open("processed_data/meta.pkl", "rb"))
@@ -24,9 +25,13 @@ class UNSW_NB15(Dataset):
             meta = {"num_clusters": None, "data_csv": None}
             
         if (meta['num_clusters'] == self.num_clusters and meta['data_csv'] == data_csv):
-            self.frame, self.cat_dicts, self.clstr_cntrs = self.recover_data()
+            self.frame, self.cat_dicts, self.clstr_cntrs, self.labels = self.recover_data()
+            print('Recovered data from cache!')
         else:
-            raw_frame = self.read_files(data_csv)
+            raw_frame, self.labels = self.read_files(data_csv, names)
+            raw_frame.drop(['sport', 'dsport', 'label'], axis=1, inplace=True)
+            self.feat_legend.drop([1, 3, 48], axis=0, inplace=True)
+
             raw_frame = self.cure_frame(raw_frame)
             self.frame, self.cat_dicts, self.clstr_cntrs = self.process_data(raw_frame)
             
@@ -34,18 +39,19 @@ class UNSW_NB15(Dataset):
             pickle.dump(meta, open("processed_data/meta.pkl", "wb"))
             pickle.dump(self.cat_dicts, open("processed_data/cat_dicts.pkl", "wb"))
             pickle.dump(self.clstr_cntrs, open("processed_data/clstr_cntrs.pkl", "wb"))
+            pickle.dump(self.labels, open("processed_data/labels.pkl", "wb"))
             self.frame.to_csv('processed_data/data.csv', index=False)
             
 
-    def read_files(self, data_csv):
-        frame = pd.read_csv(data_csv[0], header=0, dtype=object)
-        for i in range(1, len(data_csv)):
-            df = pd.read_csv(data_csv[i], header=0, names=frame.columns, dtype=object)
+    def read_files(self, data_csv, names):
+        frame = pd.DataFrame()
+        for i in range(len(data_csv)):
+            df = pd.read_csv(data_csv[i], header=0, names=names, dtype=object)
             frame = pd.concat([frame, df], axis=0)
 
-        condition = frame['CVSS'] == 'Normal'
-        frame.loc[condition, 'Attack category_x'] = 'FalsePositive'
-        return frame
+        labels = frame['label'].copy()
+        frame.loc[frame['cvss'] == 'Normal', 'attack_cat'] = 'FalsePositive'
+        return frame, labels
 
     def cure_frame(self, frame):
         dtypes = self.feat_legend['Type '].str.lower() # curing data
@@ -54,11 +60,11 @@ class UNSW_NB15(Dataset):
             frame.iloc[:, idx] = pd.to_numeric(frame.iloc[:, idx], errors='coerce') 
         return frame
 
-    def process_data(self):
+    def process_data(self, raw_frame):
         frame, cat_dicts, clstr_cntrs = raw_frame.copy(), {}, {}
         for i, ft in enumerate(tqdm(frame.columns, desc="Processing Data")):
-            type = self.feat_legend.loc[i, 'Type '].lower()
-            name = self.feat_legend.loc[i, 'Name']
+            type = self.feat_legend['Type '].iloc[i].lower()
+            name = self.feat_legend['Name'].iloc[i]
             continuous = (type != 'nominal') and (type != 'binary') and (name != 'sport') and (name != 'dsport') 
 
             unique = unique_non_null(frame[ft])
@@ -81,7 +87,8 @@ class UNSW_NB15(Dataset):
         frame = pd.read_csv('processed_data/data.csv', header=0)
         cat_dicts = pickle.load(open("processed_data/cat_dicts.pkl", "rb"))
         clstr_cntrs = pickle.load(open("processed_data/clstr_cntrs.pkl", "rb"))
-        return frame, cat_dicts, clstr_cntrs
+        labels = pickle.load(open("processed_data/labels.pkl", "rb"))
+        return frame, cat_dicts, clstr_cntrs, labels
     
     def quantizer(self, x, num_clusters):
         x = np.array(x, dtype=float).reshape(-1,1)
@@ -107,6 +114,12 @@ class UNSW_NB15(Dataset):
 
     def get_legend_frame(self):
         return self.feat_legend
+
+    def get_label_idx(self):
+        return (self.frame.columns == 'cvss') | (self.frame.columns == 'label') | (self.frame.columns == 'attack_cat')
+
+    def get_label_column(self):
+        return self.labels
 
 class ReverseTokenizer():
     def __init__(self, cat_dicts, clstr_cntrs, num_ft):

model.py
@@ -49,7 +49,7 @@ class DynamicLinear(nn.Module):
         return logits
 
 class TabMT(nn.Module):
-    def __init__(self, width, depth, heads, dropout, dim_feedforward, tu, occs, cat_dicts, num_feat):
+    def __init__(self, width, depth, heads, dropout, dim_feedforward, tu, occs, cat_dicts, num_feat, never_mask):
         super(TabMT, self).__init__()
         self.width = width
         self.depth = depth
@@ -57,6 +57,8 @@ class TabMT(nn.Module):
         self.dim_feedforward = dim_feedforward
         self.dropout = dropout
         self.tu = tu
+
+        self.never_mask = never_mask
 
         self.Embeddings, self.LinearLayers = nn.ModuleList(), nn.ModuleList()
         for idx in range(num_feat):
@@ -98,17 +100,16 @@ class TabMT(nn.Module):
     def linear(self, x, i):        
         return [self.LinearLayers[ft](x[:, ft], self.Embeddings[ft].weight()) for ft in i]
 
-    def fast_linear(self, x, i):
-        return self.LinearLayers[i](x[:, i], self.Embeddings[i].weight())
-
     def forward(self, x):
         mask = torch.rand(x.shape[1]).round().int()
+        mask[self.never_mask] = 0
+        
         i = torch.where(mask == 1)[0].tolist()
 
-        y = self.fast_embed(x, mask)
-
+        y = self.embed(x, mask)
         y = y + self.positional_encoding
         y = self.encoder(y)
+        y = self.linear(y, i)
         return y, i
 
     def gen_batch(self, x):

train.py
@@ -6,8 +6,10 @@ from model import TabMT, OrderedEmbedding
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from torch.utils.data import SubsetRandomSampler
 
-from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, accuracy_score
 import numpy as np
 
 import wandb
@@ -44,12 +46,14 @@ dataset = UNSW_NB15(data_csv=args.data_csv,
                    num_clusters=args.num_clusters)
 occs = dataset.get_cluster_centers()
 cat_dicts = dataset.get_categorical_dicts()
+never_mask = dataset.get_label_idx()
+labels = dataset.get_label_column()
 num_ft = len(occs)
 
-train_size = int(args.train_size * len(dataset))
-test_size = len(dataset) - train_size
-train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
-train_loader, test_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True), DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+train_idx, test_idx= train_test_split(np.arange(len(labels)), test_size=1-args.train_size, stratify=labels, shuffle=True, random_state=42)
+train_sampler, test_sampler = SubsetRandomSampler(train_idx), SubsetRandomSampler(test_idx)
+train_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, sampler=train_sampler)
+test_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, sampler=test_sampler)
 
 GPU_indx = 0
 device = torch.device(GPU_indx if torch.cuda.is_available() else 'cpu')
@@ -62,13 +66,14 @@ model = TabMT(width=args.width,
               dim_feedforward=args.dim_feedforward, 
               tu=[args.tu for i in range(len(occs) + len(cat_dicts))], 
               cat_dicts=cat_dicts,
-              num_feat=num_ft).to(device)
+              num_feat=num_ft,
+              never_mask=never_mask).to(device)
 model = nn.DataParallel(model)
 
 criterion = nn.CrossEntropyLoss(ignore_index=-1)
 optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
 
-class WeightedF1():
+class TrainingMetrics():
     def __init__(self, num_ft):
         self.record = [[[], []] for ft in range(num_ft)]
     
@@ -77,14 +82,13 @@ class WeightedF1():
         self.record[i][1].extend(truths.cpu().tolist())
         return None
     
-    def compute(self):
-        weightedF1 = np.array([f1_score(self.record[i][1], self.record[i][0], average='weighted') for i in range(len(self.record)) if len(self.record[i][0]) > 0])
-        
-        mean_weightedF1 = weightedF1.mean()
-        return weightedF1, mean_weightedF1
+    def compute(self): 
+        macroF1 = np.array([f1_score(self.record[i][1], self.record[i][0], average='macro') for i in range(len(self.record)) if len(self.record[i][0]) > 0])
+        accuracies = np.array([accuracy_score(self.record[i][1], self.record[i][0]) for i in range(len(self.record)) if len(self.record[i][0]) > 0])
+        return macroF1, accuracies
 
 def train(dataloader):
-    f1 = WeightedF1(num_ft)
+    tracker = TrainingMetrics(num_ft)
     total_loss = total_correct = item_count = 0
     with tqdm.tqdm(dataloader, unit="batch") as tepoch:
         for batch in tepoch:
@@ -96,12 +100,10 @@ def train(dataloader):
             y, i = model(batch)
 
             loss = 0
-            for ft_idx in i:
-                y_col = model.module.fast_linear(y, ft_idx)
-                
+            for y_col, ft_idx in zip(y, i):
                 loss += criterion(y_col, batch[:, ft_idx].long())
 
-                f1.append(y_col.argmax(dim=1), batch[:, ft_idx], ft_idx)
+                tracker.append(y_col.argmax(dim=1), batch[:, ft_idx], ft_idx)
                 total_correct += sum(y_col.argmax(dim=1) == batch[:, ft_idx]).item()
                 item_count += y_col.shape[0]
             
@@ -112,13 +114,13 @@ def train(dataloader):
             
             tepoch.set_postfix(loss=total_loss / item_count, accuracy=total_correct / item_count, num_feats=len(y))
 
-    weightedF1, mean_weightedF1 = f1.compute()
+    macroF1, accuracies = tracker.compute()
         
-    return total_loss / item_count, total_correct / item_count, weightedF1, mean_weightedF1
+    return total_loss / item_count, macroF1, accuracies
 
 def validate(dataloader):
     with torch.no_grad():
-        f1 = WeightedF1(num_ft)
+        tracker = TrainingMetrics(num_ft)
         total_loss = total_correct = item_count = 0
         with tqdm.tqdm(dataloader, unit="batch") as tepoch:
             for batch in tepoch:
@@ -128,12 +130,10 @@ def validate(dataloader):
                 y, i = model(batch)
     
                 loss = 0
-                for ft_idx in i:
-                    y_col = model.fast_linear(y, ft_idx)
-                    
+                for y_col, ft_idx in zip(y, i):
                     loss += criterion(y_col, batch[:, ft_idx].long())
     
-                    f1.append(y_col.argmax(dim=1), batch[:, ft_idx], ft_idx)
+                    tracker.append(y_col.argmax(dim=1), batch[:, ft_idx], ft_idx)
                     total_correct += sum(y_col.argmax(dim=1) == batch[:, ft_idx]).item()
                     item_count += y_col.shape[0]
         
@@ -141,9 +141,9 @@ def validate(dataloader):
                 
                 tepoch.set_postfix(loss=total_loss / item_count, accuracy=total_correct / item_count, num_feats=len(y))
             
-        weightedF1, mean_weightedF1 = f1.compute()    
+        macroF1, accuracies = tracker.compute()
         
-        return total_loss / item_count, total_correct / item_count, weightedF1, mean_weightedF1
+        return total_loss / item_count, macroF1, accuracies
 
 if (args.wandb):
     wandb.login()
@@ -155,21 +155,25 @@ save_path = 'saved_models/' + args.savename
 personal_best = 1000
 for epoch in range(args.epochs):
     model.train(True)
-    t_loss, t_acc, t_weightedF1, t_mean_weightedF1 = train(train_loader)
+    t_loss, t_macroF1, t_accuracies = train(train_loader)
     
     model.eval()
-    v_loss, v_acc, v_weightedF1, v_mean_weightedF1 = validate(test_loader)
+    v_loss, v_macroF1, v_accuracies = validate(test_loader)
     
     if (v_loss < personal_best):
         personal_best = v_loss
         torch.save(model.state_dict(), save_path)
 
     if (args.wandb):
-        wandb.log({"train_accuracy": t_acc, 
+        wandb.log({"train_mean_accuracy": t_accuracies.mean(),
+                   "train_accuracies": t_accuracies,
                    "train_loss": t_loss,
-                   "validation_accuracy": v_acc,
+                   "validation_mean_accuracy": v_accuracies.mean(),
+                   "validation_accuracies": v_accuracies,
                    "validation_loss": v_loss,
-                   "train_MWF1": t_mean_weightedF1,
-                   "validation_MWF1": v_mean_weightedF1,
+                   "train_mean_macroF1": t_macroF1.mean(),
+                   "train_macroF1s": t_macroF1,
+                   "validation_mean_macroF1": v_macroF1.mean(),
+                   "validation_macroF1s": v_macroF1,
                    "epoch": epoch})
     