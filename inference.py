switched to randomly value masking over column masking + fixed bug in gen_batch

inference.py
@@ -1,14 +1,14 @@
 from model import TabMT
 from dataset import UNSW_NB15, ReverseTokenizer
 import torch
+from torch.utils.data import DataLoader
 import torch.nn as nn
 import pickle
 import argparse
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
-
-from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from sklearn.metrics import f1_score
 
@@ -45,25 +45,21 @@ model = TabMT(width=args.width,
               dim_feedforward=args.dim_feedforward, 
               tu=[args.tu for i in range(len(occs) + len(cat_dicts))], 
               cat_dicts=cat_dicts,
-              num_feat=num_ft,
-              never_mask=[-1, -2]).to(device)
-model = nn.DataParallel(model)
+              num_feat=num_ft).to(device)
 model.load_state_dict(torch.load('saved_models/' + args.savename))
 model.eval()
 
 print('Recovered model!')
-
-x = torch.zeros((args.rows, num_ft), dtype=torch.long, device=device) - 1
-x[:, -13:] = 3 
-
-y, conf = model.module.gen_batch(x)
-    
-print(y)
-print(conf)
-    
 decoder = ReverseTokenizer(cat_dicts, occs, num_ft)
-df = decoder.decode(y.detach().cpu())
 
-print(df)
+frame = pd.DataFrame()
+for r in tqdm(range(args.rows), desc='Generating Data'):
+    x = torch.ones((1, num_ft), dtype=torch.long, device=device) * -1
+    x[:, -2] = 3
 
-# df.to_csv('synthetics/' + args.output_name, index=False)
+    y = model.gen_batch(x)
+    df = decoder.decode(y.detach().cpu())
+    
+    frame = pd.concat([frame, df])
+    
+print(frame)

model.py
@@ -49,7 +49,7 @@ class DynamicLinear(nn.Module):
         return logits
 
 class TabMT(nn.Module):
-    def __init__(self, width, depth, heads, dropout, dim_feedforward, tu, occs, cat_dicts, num_feat, never_mask):
+    def __init__(self, width, depth, heads, dropout, dim_feedforward, tu, occs, cat_dicts, num_feat):
         super(TabMT, self).__init__()
         self.width = width
         self.depth = depth
@@ -57,8 +57,6 @@ class TabMT(nn.Module):
         self.dim_feedforward = dim_feedforward
         self.dropout = dropout
         self.tu = tu
-
-        self.never_mask = never_mask
 
         self.Embeddings, self.LinearLayers = nn.ModuleList(), nn.ModuleList()
         for idx in range(num_feat):
@@ -78,36 +76,32 @@ class TabMT(nn.Module):
         self.mask_vec = nn.Parameter(standard_norm.sample((1, self.width)))
         self.positional_encoding = nn.Parameter(position_norm.sample((num_feat, self.width)))
     
-    def embed(self, x, mask):
+    def embed(self, x, mask):        
         out = self.mask_vec.repeat(x.shape[0], x.shape[1], 1)
+        
         for ft in range(x.shape[1]):
-            col_mask = (mask[ft] == 0) & (x[:, ft] != -1)
+            col_mask = (mask[:, ft] == 0) & (x[:, ft] != -1)
             out[col_mask, ft] = self.Embeddings[ft](x[col_mask, ft])
         return out
 
-    def linear(self, x, i):        
-        return [self.LinearLayers[ft](x[:, ft], self.Embeddings[ft].weight()) for ft in i]
+    def linear(self, x):        
+        return [self.LinearLayers[ft](x[:, ft], self.Embeddings[ft].weight()) for ft in range(x.shape[1])]
 
     def forward(self, x):        
-        mask = torch.rand(x.shape[1]).round().int().to(x.device)
-        mask[self.never_mask] = 0
-        
-        i = torch.where(mask == 1)[0].tolist()
-        
+        mask = torch.rand(x.size(), device=x.device).round().int()
         y = self.embed(x, mask)
         y = y + self.positional_encoding
         y = self.encoder(y)
-        y = self.linear(y, i)
-        return y, i
+        y = self.linear(y)
+        return y, mask
 
     def gen_batch(self, x):
         for i in torch.randperm(x.shape[1]):
-            y = self.embed(x, torch.ones(x.shape[1]))
+            y = self.embed(x, torch.zeros(x.size(), device=x.device))
+            
             y = y + self.positional_encoding
             y = self.encoder(y)
-            y = self.linear(y, [i])
-    
-            missing = torch.where(x[:, i] == -1)[0]
-            x[missing, i] = y[0][missing].argmax(dim=1)
-        
+            y = self.LinearLayers[i](y[:, i], self.Embeddings[i].weight())
+            
+            x[x[:, i] == -1, i] = y[x[:, i] == -1].argmax(dim=1)        
         return x

train.py
@@ -64,9 +64,8 @@ model = TabMT(width=args.width,
               dim_feedforward=args.dim_feedforward, 
               tu=[args.tu for i in range(len(occs) + len(cat_dicts))], 
               cat_dicts=cat_dicts,
-              num_feat=num_ft,
-              never_mask=never_mask).to(device)
-model = nn.DataParallel(model)
+              num_feat=num_ft).to(device)
+# model = nn.DataParallel(model)
 
 criterion = nn.CrossEntropyLoss(ignore_index=-1)
 optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
@@ -76,8 +75,11 @@ class TrainingMetrics():
         self.record = [[[], []] for ft in range(num_ft)]
     
     def append(self, predictions, truths, i):
-        self.record[i][0].extend(predictions.detach().cpu().tolist())
-        self.record[i][1].extend(truths.cpu().tolist())
+        p = predictions.detach().cpu().tolist()
+        t = truths.detach().cpu().tolist()
+        
+        self.record[i][0].extend(p)
+        self.record[i][1].extend(t)
         return None
     
     def compute(self): 
@@ -95,15 +97,22 @@ def train(dataloader):
             batch = batch.to(device)
             optimizer.zero_grad()
 
-            y, i = model(batch)
+            y, mask = model(batch)
 
             loss = 0
-            for y_col, ft_idx in zip(y, i):
-                loss += criterion(y_col, batch[:, ft_idx].long())
-
-                tracker.append(y_col.argmax(dim=1), batch[:, ft_idx], ft_idx)
-                total_correct += sum(y_col.argmax(dim=1) == batch[:, ft_idx]).item()
-                item_count += y_col.shape[0]
+            for ft, y_ft in enumerate(y):
+                truth = batch[mask[:, ft], ft]
+                pred = y_ft[mask[:, ft]]
+                
+                pred = pred[truth != -1]
+                truth = truth[truth != -1]
+                
+                if (len(truth) > 0):
+                    loss += criterion(pred, truth.long())
+                
+                    tracker.append(pred.argmax(dim=1), truth, ft)
+                    total_correct += sum(pred.argmax(dim=1) == truth).item()
+                    item_count += pred.shape[0]
             
             loss.backward()
             optimizer.step()
@@ -125,15 +134,22 @@ def validate(dataloader):
                 tepoch.set_description(f"Validation Epoch {epoch}") 
     
                 batch = batch.to(device)
-                y, i = model(batch)
+                y, mask = model(batch)
     
                 loss = 0
-                for y_col, ft_idx in zip(y, i):
-                    loss += criterion(y_col, batch[:, ft_idx].long())
-    
-                    tracker.append(y_col.argmax(dim=1), batch[:, ft_idx], ft_idx)
-                    total_correct += sum(y_col.argmax(dim=1) == batch[:, ft_idx]).item()
-                    item_count += y_col.shape[0]
+                for ft, y_ft in enumerate(y):
+                    truth = batch[mask[:, ft], ft]
+                    pred = y_ft[mask[:, ft]]
+                
+                    pred = pred[truth != -1]
+                    truth = truth[truth != -1]
+                    
+                    if (len(truth) > 0):
+                        loss += criterion(pred, truth.long())
+                
+                        tracker.append(pred.argmax(dim=1), truth, ft)
+                        total_correct += sum(pred.argmax(dim=1) == truth).item()
+                        item_count += pred.shape[0]
         
                 total_loss += loss.item()
                 
@@ -155,13 +171,15 @@ personal_best = 1000
 for epoch in range(args.epochs):
     model.train(True)
     t_loss, t_macroF1, t_accuracies = train(train_loader)
-    
-    test_input = torch.zeros((5, num_ft), dtype=torch.long, device=device) - 1
-    test_input[:, never_mask] = 0
-    print(model.module.gen_batch(test_input))
+    print(f't_accuracy: {t_accuracies.mean()}, t_F1: {t_macroF1.mean()}')
     
     model.eval()
+    
+    test_input = torch.zeros((5, num_ft), dtype=torch.long, device=device) - 1
+    print(model.gen_batch(test_input))
+    
     v_loss, v_macroF1, v_accuracies = validate(test_loader)
+    print(f't_accuracy: {v_accuracies.mean()}, t_F1: {v_macroF1.mean()}')
     
     if (v_loss < personal_best):
         personal_best = v_loss